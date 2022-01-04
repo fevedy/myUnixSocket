86eee28e4f86bd0beb45b094b59ba085dbcc2e00
fevedy <atuufeve@qq.com>
2022-01-04 18:14:46 +0800

    第一次提交文件，已经调试通过的unix 测试程序

---
+++ server.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/socket.domain"
+
+void err_quit(char *estr)
+{
+    perror(estr);
+    exit(-1);
+}
+
+int main(int argc,char *argv[])
+{
+    char                  buf[1024];
+    int                   i;
+    int                   listen_fd = -1;
+    int                   client_fd;
+    int                   rv = -1;
+    struct sockaddr_un    servaddr;   //unix域socket套接字
+    struct sockaddr_un    cliaddr;
+    socklen_t             addrlen = sizeof(servaddr);
+
+    listen_fd = socket(AF_UNIX,SOCK_STREAM,0);
+    if(listen_fd < 0)
+    {
+        err_quit("socket create fail");
+    }
+
+    printf("create sockfd[%d] ok!\n",listen_fd);
+
+    if(!access(SOCKET_PATH,F_OK)) 
+    {
+        remove(SOCKET_PATH);
+    }
+
+    bzero(&servaddr,addrlen);
+
+    servaddr.sun_family = AF_UNIX;
+    strncpy(servaddr.sun_path,SOCKET_PATH,sizeof(servaddr.sun_path));
+
+    if(bind(listen_fd,(struct sockaddr *)&servaddr,addrlen) < 0)
+    {
+        printf("Create socket failure:%s\n",strerror(errno));
+        unlink(SOCKET_PATH);
+        return -1;
+    }
+
+    listen(listen_fd,13);
+
+        printf("Start waiting and accept new client connect......\n");
+        client_fd = accept(listen_fd,(struct sockaddr *)&cliaddr,&addrlen);
+        if(client_fd < 0)
+        {
+            printf("Accept new client failure:%s\n",strerror(errno));
+            return -2;
+        }
+    while(1)
+    {
+
+        memset(buf,0,sizeof(buf));
+	strcpy( buf, "hello world");
+
+        if(write(client_fd,buf,strlen( buf)) < 0)
+        {
+            printf("Write to client[%d] failure:%s\n",client_fd,strerror(errno));
+            close(client_fd);
+            continue;
+        }
+
+        printf("Write bytes data to client[%d]\n",client_fd);
+
+//        close(client_fd);
+        
+        sleep(1);
+    }
+
+    close(listen_fd);
+
+    
+}
